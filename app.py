d7c305fecb92d146b94f53b9261f213423e763d2
Vijay Kandpal <72745130+vijayskandpal@users.noreply.github.com>
2024-02-21 11:53:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,14 @@ FilePath = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vT-g-e-JRzeuReeaIvjf
 df_data = pd.read_excel(FilePath,sheet_name='DATA', usecols=range(0,10))
 df_data.rename(columns={'Bill / Inv No /Faulty/Sample': 'INV'}, inplace=True)
 
+# Downcast numeric types
+numeric_columns = df_data.select_dtypes(include=['int64', 'float64']).columns
+df_data[numeric_columns] = df_data[numeric_columns].apply(pd.to_numeric, downcast='unsigned')
+
+# Convert object types to categorical
+object_columns = df_data.select_dtypes(include=['object']).columns
+df_data[object_columns] = df_data[object_columns].astype('category')
+
 #st.dataframe(df_data)
 df_clstk = pd.read_excel(FilePath,sheet_name='Item_List', usecols=range(0,11))
 df_clstk = df_clstk.drop(columns=['Box Location', 'Physical Date','Net'])
